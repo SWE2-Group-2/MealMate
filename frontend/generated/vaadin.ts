82d3e8ba3242fd8e1c2951fb7274dba3ddc3aec3
Roland Indra <ir4465@mci4me.at>
2024-04-26 18:23:52 +0200

    SImple Commit after Pull

--- frontend/generated/vaadin.ts
+++
@@ -1,7 +0,0 @@
-import './vaadin-featureflags.js';
-
-import './index';
-
-import './theme-mealmate.global.generated.js';
-import { applyTheme } from './theme.js';
-applyTheme(document);
--- src/main/java/mci/softwareengineering2/group2/views/MainLayout.java
+++ src/main/java/mci/softwareengineering2/group2/views/MainLayout.java
@@ -42,7 +42,7 @@ public class MainLayout extends AppLayout {
     }
 
     private void addDrawerContent() {
-        H1 appName = new H1("My App");
+        H1 appName = new H1("MealMate");
         appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
         Header header = new Header(appName);
 
